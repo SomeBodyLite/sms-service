f420b04af9ed0cbe5332e756fb1db8ee02c96fad
SomeBodyLite <danhik.maksamov@yandex.ru>
2024-01-08 02:04:11 +0600

    v.1.1.4
    minor fixes & timer

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -39,3 +39,38 @@ function setCountdown(targetDate) {
 }
 // Вызов таймера. Год-месяц-дата чч:мм:сс
 setCountdown("2023-12-26 15:10:00");
+
+
+// function setCountdown(targetDate) {
+//   // Получаем дату окончания в миллисекундах с учетом UTC
+//   var endDate = new Date(targetDate + 'Z').getTime();
+
+//   var x = setInterval(function () {
+//     // Получаем текущее время в миллисекундах с учетом UTC
+//     var now = new Date().getTime();
+
+//     var distance = endDate - now;
+
+//     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+//     var formattedTime = formatNumber(minutes) + ":" + formatNumber(seconds) + " left";
+//     console.log("(" + formatNumber(days) + ":" + formatNumber(hours) + ":" + formatNumber(minutes) + ":" + formatNumber(seconds) + ")");
+
+//     $(".time").html(formattedTime);
+
+//     if (distance < 0) {
+//       clearInterval(x);
+//       $(".time").html("00:00 left");
+//     }
+//   }, 1000);
+
+//   function formatNumber(number) {
+//     return number < 10 ? "0" + number : number;
+//   }
+// }
+
+// // Пример вызова функции с передачей даты
+// setCountdown("2024-01-07T20:43:00");
